091fa4020e334728740234b750c8756d0e77e2ba
jason <jasonkellogglee@gmail.com>
2019-05-13 20:47:54 -0700

    Updates

--- clicky-game-of-thrones/README.md
+++
@@ -1,7 +0,0 @@
-## Clicky Game of Thrones!
-
-Welcome to Clicky Game of Thrones!  
-
-This is a simple memory card game created in React.  
-
-Don't click the same card twice, and try to beat your top score!
---
+++ precache-manifest.e986afbd517c7675ae6cd4cf4838a6a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbed551ba2df4976bf61474bff9dcbd",
+    "url": "/clicky-game-of-thrones/index.html"
+  },
+  {
+    "revision": "3bf72b061b485fea6110",
+    "url": "/clicky-game-of-thrones/static/css/main.3acde289.chunk.css"
+  },
+  {
+    "revision": "e7535b161eb112a5096e",
+    "url": "/clicky-game-of-thrones/static/js/2.dafcb6ff.chunk.js"
+  },
+  {
+    "revision": "3bf72b061b485fea6110",
+    "url": "/clicky-game-of-thrones/static/js/main.1d0f0705.chunk.js"
+  },
+  {
+    "revision": "4f03e8d67bce91cda643",
+    "url": "/clicky-game-of-thrones/static/js/runtime~main.cd2e6721.js"
+  }
+]);
